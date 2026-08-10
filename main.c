9da01f56a12a6a78681ea92d29a33583a1376b31
Mahir Ahmed <185059260+mahirisavailable@users.noreply.github.com>
2026-08-10 18:12:39 +0600

    Refactor main function with menu and restart logic
    Refactor main function to include a menu and restart logic. Add new textures for logo and play button, and implement countdown before starting the game.

--- main.c
+++ main.c
@@ -7,51 +7,23 @@
 #define space 100
 #define speed 250
 
-char maze[31][30] = {
-    "############################", // 0
-    "#............##............#", // 1
-    "#.####.#####.##.#####.####.#", // 2
-    "#o#  #.#   #.##.#   #.#  #o#", // 3
-    "#.####.#####.##.#####.####.#", // 4
-    "#..........................#", // 5
-    "#.####.##.########.##.####.#", // 6
-    "#.####.##.########.##.####.#", // 7
-    "#......##....##....##......#", // 8
-    "######.##### ## #####.######", // 9
-    "     #.##### ## #####.#     ", // 10
-    "     #.##          ##.#     ", // 11
-    "     #.## ###--### ##.#     ", // 12
-    "######.## #      # ##.######", // 13
-    "      .   #      #   .      ", // 14
-    "######.## #      # ##.######", // 15
-    "     #.## ######## ##.#     ", // 16
-    "     #.##          ##.#     ", // 17
-    "     #.## ######## ##.#     ", // 18
-    "######.## ######## ##.######", // 19
-    "#............##............#", // 20
-    "#.####.#####.##.#####.####.#", // 21
-    "#.####.#####.##.#####.####.#", // 22
-    "#o..##.......  .......##..o#", // 23
-    "###.##.##.########.##.##.###", // 24
-    "###.##.##.########.##.##.###", // 25
-    "#......##....##....##......#", // 26
-    "#.##########.##.##########.#", // 27
-    "#.##########.##.##########.#", // 28
-    "#..........................#", // 29
-    "############################", // 30
-};
-
 int main(void)
 {
     InitWindow(col * 25 + 2 * space, row * 25 + 1.5 * space, "PacMan");
     InitAudioDevice();
     SetTargetFPS(60);
 
+    int width = col * 25 + 2 * space;
+    int height = row * 25 + 1.5 * space;
+
     // Texture Loading
     Texture2D pac_left[3], pac_right[3], pac_up[3], pac_down[3], bg, idle;
     bg = LoadTexture("assets/bg.png");
     idle = LoadTexture("assets/idle.png");
     Texture2D apple = LoadTexture("assets/other/apple.png");
+    Texture2D logo = LoadTexture("assets/pacman-logo.png");
+    Texture2D play = LoadTexture("assets/play-button.png");
+    Texture2D hudai = LoadTexture("assets/other/hudai.png");
     for (int i = 0; i < 3; i++)
     {
         pac_left[i] = LoadTexture(TextFormat("assets/pacman-left/%d.png", i + 1));
@@ -64,17 +36,56 @@ int main(void)
     Sound chomp = LoadSound("assets/audio/pacman_chomp.wav");
     Sound eatfruit = LoadSound("assets/audio/pacman_eatfruit.wav");
 
+    int point = 0;
+    bool menu = true;
+
+restart:
+    char maze[31][30] = {
+        "############################", // 0
+        "#............##............#", // 1
+        "#.####.#####.##.#####.####.#", // 2
+        "#o#  #.#   #.##.#   #.#  #o#", // 3
+        "#.####.#####.##.#####.####.#", // 4
+        "#..........................#", // 5
+        "#.####.##.########.##.####.#", // 6
+        "#.####.##.########.##.####.#", // 7
+        "#......##....##....##......#", // 8
+        "######.##### ## #####.######", // 9
+        "     #.##### ## #####.#     ", // 10
+        "     #.##          ##.#     ", // 11
+        "     #.## ###--### ##.#     ", // 12
+        "######.## #      # ##.######", // 13
+        "      .   #      #   .      ", // 14
+        "######.## #      # ##.######", // 15
+        "     #.## ######## ##.#     ", // 16
+        "     #.##          ##.#     ", // 17
+        "     #.## ######## ##.#     ", // 18
+        "######.## ######## ##.######", // 19
+        "#............##............#", // 20
+        "#.####.#####.##.#####.####.#", // 21
+        "#.####.#####.##.#####.####.#", // 22
+        "#o..##.......  .......##..o#", // 23
+        "###.##.##.########.##.##.###", // 24
+        "###.##.##.########.##.##.###", // 25
+        "#......##....##....##......#", // 26
+        "#.##########.##.##########.#", // 27
+        "#.##########.##.##########.#", // 28
+        "#..........................#", // 29
+        "############################", // 30
+    };
+
     // Variables
     const Vector2 origin = {0, 0};
     Vector2 pac_pos = {13 * 25 + space, 23 * 25 + space};
     Vector2 pac_speed = {0, 0};
+
     // collision with maze
     char *nextmove = "null";
 
-    int point = 0;
     int dots = 240;
     int bigdots = 4;
     float appletime;
+    float countdown;
 
     while (!WindowShouldClose())
     {
@@ -85,6 +96,21 @@ int main(void)
         int curr = GetTime() * 15;
         float currenttime = GetTime();
         float alpha = (sinf(curr / 2) + 1.0f) / 2.0f;
+
+        if (menu)
+        {
+            DrawTexturePro(logo, (Rectangle){0, 0, logo.width, logo.height}, (Rectangle){space, space, col * 25, 200}, origin, 0, WHITE);
+            DrawTexturePro(hudai, (Rectangle){0, 0, hudai.width, hudai.height}, (Rectangle){space, height - 350 - space, 400, 400}, origin, 0, WHITE);
+            Rectangle button = {width / 2 - 125, height / 2 - 100, 250, 100};
+            DrawTexturePro(play, (Rectangle){0, 0, play.width, play.height}, button, origin, 0, WHITE);
+            if (CheckCollisionPointRec(GetMousePosition(), button) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            {
+                menu = false;
+                countdown = currenttime;
+            }
+            EndDrawing();
+            continue;
+        }
 
         DrawTexturePro(bg, (Rectangle){0, 0, bg.width, bg.height}, (Rectangle){space, space - 20, col * 25, row * 25 + 15}, origin, 0, WHITE);
         DrawText(TextFormat("Points: %d", point), 500, 50, 50, RAYWHITE);
@@ -116,6 +142,8 @@ int main(void)
         }
         if (currenttime - appletime < 8 && maze[17][13] == 'a')
             DrawTexturePro(apple, (Rectangle){0, 0, apple.width, apple.height}, (Rectangle){13 * 25 + space - 5 + 12.5, 17 * 25 + space - 5, 35, 35}, origin, 0, WHITE);
+        if (currenttime - appletime >= 8 && maze[17][13] == 'a')
+            maze[17][13] = 'A';
 
         // Direction input
         if (IsKeyPressed(KEY_LEFT))
@@ -186,11 +214,22 @@ int main(void)
         }
 
         // Position Update
-        pac_pos = Vector2Add(pac_pos, Vector2Scale(pac_speed, dt));
-        if (pac_pos.x + 12.5 < space)
-            pac_pos.x += col * 25;
-        else if (pac_pos.x + 12.5 > space + col * 25)
-            pac_pos.x -= col * 25;
+        if ((currenttime - countdown) < 4)
+        {
+            int cd = 3 - (int)(currenttime - countdown);
+            if (cd)
+                DrawText(TextFormat("%d", cd), width / 2 - 10, 17 * 25 + space - 10, 50, YELLOW);
+            else
+                DrawText("READY", width / 2 - 65, 17 * 25 + space - 5, 40, YELLOW);
+        }
+        else
+        {
+            pac_pos = Vector2Add(pac_pos, Vector2Scale(pac_speed, dt));
+            if (pac_pos.x + 12.5 < space)
+                pac_pos.x += col * 25;
+            else if (pac_pos.x + 12.5 > space + col * 25)
+                pac_pos.x -= col * 25;
+        }
 
         // Draw PacMan
         Rectangle pacpac = {pac_pos.x - 5, pac_pos.y - 5, 35, 35};
@@ -217,6 +256,11 @@ int main(void)
         }
 
         EndDrawing();
+        if (dots == 0 & bigdots == 0)
+        {
+            countdown = currenttime;
+            goto restart;
+        }
     }
 
     // Unload Texture
@@ -230,6 +274,9 @@ int main(void)
     UnloadTexture(bg);
     UnloadTexture(idle);
     UnloadTexture(apple);
+    UnloadTexture(logo);
+    UnloadTexture(play);
+    UnloadTexture(hudai);
 
     // Unload Sound
     UnloadSound(chomp);
